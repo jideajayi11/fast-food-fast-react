c9a4c8e041308bacf1c46f673055e4357cf0ee85
jideajayi11 <jideajayi11@gmail.com>
2019-02-04 20:31:59 +0100

    [Chore #163706674] add heroku script

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname)));
 app.all('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+ res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port);
